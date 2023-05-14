d0913456e185515b9a01b1f6a5420f9478bd8d35
thekolesnikov <thesharkov@gmail.com>
2023-05-14 21:57:27 +0300

    add Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,18 @@
+import { NavLink, Link } from 'react-router-dom';
+import styles from './Header.module.css';
+
 function Header() {
-    return <div>Header</div>;
+    return (
+        <header className={styles.header}>
+            <div className={styles.header__title}>
+                <Link to="/">VIN Decoder</Link>
+            </div>
+            <div className={styles.header__links}>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/variables">Variables</NavLink>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
